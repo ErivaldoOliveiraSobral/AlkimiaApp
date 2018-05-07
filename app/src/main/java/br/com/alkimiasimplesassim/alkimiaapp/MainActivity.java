953a09e0f9311703ea54bbad702c44960b2236be
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-05-07 20:19:15 -0300

    Autenticação

--- app/src/main/java/br/com/alkimiasimplesassim/alkimiaapp/MainActivity.java
+++ app/src/main/java/br/com/alkimiasimplesassim/alkimiaapp/MainActivity.java
@@ -1,13 +1,44 @@
 package br.com.alkimiasimplesassim.alkimiaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsuario;
+    private EditText txtSenha;
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEntrar =(Button) findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(onClickLogin());
+    }
+
+    private View.OnClickListener onClickLogin() {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                txtUsuario =(EditText) findViewById(R.id.txtUsuario);
+                txtSenha =(EditText) findViewById(R.id.txtSenha);
+
+                String nome = txtUsuario.getText().toString();
+                String senha = txtSenha.getText().toString();
+
+                //Toast.makeText(MainActivity.this, "Usuário: " + nome + "; Senha: " + senha, Toast.LENGTH_LONG).show();
+                if (nome.equals("admin") && senha.equals("1234")) {
+                    Intent intent = new Intent(MainActivity.this, TelaInicialActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };
     }
 }
